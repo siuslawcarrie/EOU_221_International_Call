8afdfd24b9adf09451e7d4c7d56d13583e81e039
siuslawcarrie <102423932+siuslawcarrie@users.noreply.github.com>
2023-10-22 14:40:06 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/*The cost of an international call from New York to New Delhi is calculated as follows:
+ * Connection fee: $1.99, $2.00 for the first three minutes,
+ * and $0.45 for each additional minute.
+ * Write a program that prompts the user to enter the number of
+ * minutes the call lasted and outputs the amount due.
+ * Format your output with two decimal places.
+ * */
+
+/*
+ * const CONN = 1.99
+ * const THREEMIN = 2.00
+ * const ADDLMIN = 0.45
+ *
+ * int main
+ * user input msg
+ * int call_minutes = user input
+ * call amount due function
+ * amount due = amount due function(call minutes)
+ * display amount due
+ *
+ * //END MAIN
+ *
+ * //FUNCTION
+ * amount due function
+ * takes call minutes
+ * float amount due
+ *
+ * amount due = CONN + THREEMIN
+ * IF user minutes > 3
+ *amount due += (user min - 3)*0.45
+ * amount due
+ *
+ *
+ */
